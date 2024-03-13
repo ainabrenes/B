571162fdaa2dd98066d5cd6fd3401ec9769f4a96
Aina <abrenes@nigul.cide.es>
2024-03-13 16:39:33 +0100

    actividad

---
+++ app/src/main/java/b/Usuario.java
@@ -0,0 +1,42 @@
+package b;
+import java.util.*;
+public class Usuario {
+    public List<Usuario> usuarios = new ArrayList<>();
+   
+    private int id;
+    private String nombre;
+    private String correo;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() { 
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Usuario(String nombre,String correo,int id){
+        this.id=id;
+        this.nombre=nombre;
+        this.correo=correo;
+    }
+    
+
+        
+}
